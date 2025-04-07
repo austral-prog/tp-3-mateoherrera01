9a1dfc11c3a781430087fd24c8a28c5710e33baf
mateoherrera01 <maherrera@mail.austral.edu.ar>
2025-04-07 15:11:23 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -5,7 +5,7 @@ def check_vowels():
     print(f"Contiene a:", "a" in name.lower())
     print(f"Contiene e:", "e" in name.lower())
     print(f"Contiene i:", "i" in name.lower())
-    print(f"Contiene o:", "a" in name.lower())
+    print(f"Contiene o:", "o" in name.lower())
     print(f"Contiene u:", "u" in name.lower())
 
 # Para verificar este ejercicio ejecutar el comando
